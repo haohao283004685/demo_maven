a86d209ce94d31eb3b9a3a925005b160cc57dda5
changyang <changyang@q.com>
2019-02-22 16:06:06 +0800

    没用

---
+++ pojo/src/main/java/com/test/User.java
@@ -0,0 +1,34 @@
+package com.test;
+
+public class User {
+    private String username;
+    private String userage;
+    private String user;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getUserage() {
+        return userage;
+    }
+
+    public User setUserage(String userage) {
+        this.userage = userage;
+        return this;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public User setUser(String user) {
+        this.user = user;
+        return this;
+    }
+}
